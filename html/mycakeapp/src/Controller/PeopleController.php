bcfcf68aefc6c51df281ad40613ff6f3edc485b6
takumi <takumik9823@gmail.com>
2020-10-13 00:22:57 +0900

    [Chapter-3] エンティティの作成

--- html/mycakeapp/src/Controller/PeopleController.php
+++ html/mycakeapp/src/Controller/PeopleController.php
@@ -6,10 +6,24 @@ use App\Controller\AppController;
 class PeopleController extends AppController {
 
 	public function index() {
-    $id = $this->request->query['id'];
-    $data = $this->People->get($id);
-    $this->set('data', $data);
+		$data = $this->People->find('all');
+		$this->set('data', $data);
+	}
+
+	public function add() {
+		$entity = $this->People->newEntity();
+		$this->set('entity', $entity);
+	}
+
+	public function create() {
+		if ($this->request->is('post')){
+			$data =  $this->request->data['People'];
+			$entity = $this->People->newEntity($data);
+			$this->People->save($entity);
+		}
+		return $this->redirect(['action'=>'index']);
 	}
 
 }
+
 
--- html/mycakeapp/src/Template/People/index.ctp
+++ html/mycakeapp/src/Template/People/index.ctp
@@ -1,4 +1,14 @@
 <p>This is People table records.</p>
-<pre>
-<?php print_r($data->toArray()); ?>
-</pre>
+<table>
+<thead><tr>
+	<th>id</th><th>name</th><th>mail</th><th>age</th>
+</tr></thead>
+<?php foreach($data->toArray() as $obj): ?>
+<tr>
+	<td><?=h($obj->id) ?></td>
+	<td><?=h($obj->name) ?></td>
+	<td><?=h($obj->mail) ?></td>
+	<td><?=h($obj->age) ?></td>
+</tr>
+<?php endforeach; ?>
+</table>
