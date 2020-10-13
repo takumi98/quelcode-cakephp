e4456118d225382a7a95e731f884282cd2251c2e
takumi <takumik9823@gmail.com>
2020-10-13 14:24:30 +0900

    edit.ctpの作成, PeopleControllerの編集

--- html/mycakeapp/src/Controller/PeopleController.php
+++ html/mycakeapp/src/Controller/PeopleController.php
@@ -8,7 +8,23 @@ class PeopleController extends AppController {
 	public function index() {
 		$data = $this->People->find('all');
 		$this->set('data', $data);
-	}
+  }
+  
+  public function edit() {
+    $id = $this->request->query['id'];
+    $entity = $this->People->get($id);
+    $this->set('entity', $entity);
+  }
+  
+  public function update() {
+    if ($this->request->is('post')){
+      $data =  $this->request->data['People'];
+      $entity = $this->People->get($data['id']);
+      $this->People->patchEntity($entity, $data);
+      $this->People->save($entity);
+    }
+    return $this->redirect(['action'=>'index']);
+  }
 
 	public function add() {
 		$entity = $this->People->newEntity();
